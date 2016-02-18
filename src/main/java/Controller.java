c278e57e9e25b70ab4ca4799b58752730b4537f4
Pavel Yurkin <woodythegreat@yandex.ru>
2016-02-18 17:45:04 +0300

    Edit Controller using new classes

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,30 +1,20 @@
 import composer.ComposeBlockProvider;
 import composer.CompositionComposer;
-import composer.first.FirstBlockProvider;
-import composer.next.NextBlockProvider;
-import composer.next.filter.*;
-import composer.step.CompositionStep;
+import composer.next.FormNextBlockProvider;
+import composer.next.filter.ComposeBlockFilter;
+import composer.next.filter.custom.BachChoralFilter;
 import decomposer.CompositionDecomposer;
 import jm.JMC;
 import jm.util.Write;
-import model.ComposeBlock;
 import model.Lexicon;
 import model.composition.Composition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import persistance.dao.LexiconDAO;
-import persistance.dao.LexiconDAO_database;
-import persistance.dao.LexiconDAO_stub;
 import utils.CompositionLoader;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-
-import static jm.JMC.*;
 
 /**
  * Created by night wish on 24.01.2016.
@@ -38,10 +28,10 @@ public class Controller {
 	private CompositionDecomposer compositionDecomposer = applicationContext.getBean( CompositionDecomposer.class );
 
 	public static void main( String... args ) throws IOException {
-		new Controller().getRealPieceTest();
+		new Controller().getRealPiece();
 	}
 
-	public void getRealPieceTest() throws IOException {
+	public void getRealPiece() throws IOException {
 		//				List< Composition > compositionList = compositionLoader.getCompositions(
 		//				  new File( "src\\test\\decomposer\\form\\formDecomposer\\quartets\\2.Scarecrow's song (midi).mid" ),
 		//				  		  new File( "src\\test\\decomposer\\form\\formDecomposer\\quartets\\2.biosphere(midi).mid" ),
@@ -55,38 +45,15 @@ public class Controller {
 		Lexicon lexicon = compositionDecomposer.decompose( compositionList, JMC.WHOLE_NOTE );
 //		compositionDecomposer.getLexiconDAO().persist( lexicon );
 
-		FirstBlockProvider firstStepProvider = new FirstBlockProvider() {
-			@Override
-			public Optional<ComposeBlock> getFirstBlock( Lexicon lexicon, List<ComposeBlock> exclusions ) {
-				ComposeBlock composeBlock1 = lexicon.getComposeBlockList().stream().filter( composeBlock -> !exclusions.contains( composeBlock ) ).findFirst()
-						.get();
-				return Optional.ofNullable( composeBlock1 );
-			}
-		};
+		ComposeBlockFilter bachChoralFilter = applicationContext.getBean( BachChoralFilter.class );
+		FormNextBlockProvider formNextBlockProvider = applicationContext.getBean( FormNextBlockProvider.class );
+//		formNextBlockProvider.setComposeBlockFilter( bachChoralFilter );
 
-		NextBlockProvider nextBlockProvider = new NextBlockProvider() {
-			@Override
-			public Optional<ComposeBlock> getNextBlock( List<CompositionStep> previousCompositionSteps ) {
-				CompositionStep lastCompositionStep = previousCompositionSteps.get( previousCompositionSteps.size() - 1 );
-				List<ComposeBlock> possibleNextComposeBlocks = new ArrayList<>( lastCompositionStep.getOriginComposeBlock().getPossibleNextComposeBlocks() );
-				possibleNextComposeBlocks.removeAll( lastCompositionStep.getNextMusicBlockExclusions() );
-
-				ComposeBlockFilter composeBlockFilter = new ComposeBlockVarietyFilter( 6,
-						new ComposeBlockRestFilter( QUARTER_NOTE,
-								new ComposeBlockVoiceRangeFilter( Arrays.asList(
-										new ComposeBlockVoiceRangeFilter.Range( C4, C6 ),
-										new ComposeBlockVoiceRangeFilter.Range( F3, F5 ),
-										new ComposeBlockVoiceRangeFilter.Range( A2, A4 ),
-										new ComposeBlockVoiceRangeFilter.Range( F2, F4 )
-								) ) ) );
-				Optional<ComposeBlock> lastOfPossibles = composeBlockFilter.filter( possibleNextComposeBlocks, previousCompositionSteps ).stream()
-						.reduce( ( composeBlock1, composeBlock2 ) -> composeBlock2 );
-				return lastOfPossibles;
-			}
-		};
+		ComposeBlockProvider composeBlockProvider = applicationContext.getBean( ComposeBlockProvider.class );
+		composeBlockProvider.setNextBlockProvider( formNextBlockProvider );
 
 		Composition composition = compositionComposer
-				.compose( new ComposeBlockProvider( firstStepProvider, nextBlockProvider ), lexicon, "ABCD", 32 * JMC.WHOLE_NOTE );
+				.compose( composeBlockProvider, lexicon, "ABCD", 16 * JMC.WHOLE_NOTE );
 		//		assertEquals( 16., composition.getEndTime(), 0 );
 
 		//		View.notate( composition );
