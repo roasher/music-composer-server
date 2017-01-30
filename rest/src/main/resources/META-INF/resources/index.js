bf0e1971c23b8a6c404a40930365634eb534b32f
pavel yurkin <pavel.yurkin@leroymerlin.ru>
2017-01-30 10:28:38 +0300

    add possibility to play several parts

--- rest/src/main/resources/META-INF/resources/index.js
+++ rest/src/main/resources/META-INF/resources/index.js
@@ -1,11 +1,17 @@
-var playNextTBars = function (amountOfBarsToLoad, whenShouldPlay) {
+var playNextTBars = function (compositionId, amountOfBarsToLoad, player) {
     console.log("retrieving notes for " + amountOfBarsToLoad + " amountOfBarsToLoad");
     var request = new XMLHttpRequest();
-    request.open("GET", "/getNotes?number=" + amountOfBarsToLoad, true);
+    request.open("GET", "/getBars?compositionId=" + compositionId + "&numberOfBars=" + amountOfBarsToLoad, true);
     request.addEventListener("load", function () {
-        var responsedata = eval("(" + request.responseText + ")");
-        for (var noteNumber = 0; noteNumber < responsedata.length; noteNumber++) {
-            player.play(responsedata[noteNumber].pitch, responsedata[noteNumber].rhythmValue, true)
+        var notes = eval("(" + request.responseText + ")").notes;
+        console.log(notes);
+        for (var instrumentNumber = 0; instrumentNumber < notes.length; instrumentNumber++) {
+            for (var noteNumber = 0; noteNumber < notes[instrumentNumber].length; noteNumber++ ) {
+                player.partPlayers[instrumentNumber].play(
+                    notes[instrumentNumber][noteNumber].pitch,
+                    notes[instrumentNumber][noteNumber].rhythmValue,
+                    true);
+            }
         }
     });
     request.send(null);
