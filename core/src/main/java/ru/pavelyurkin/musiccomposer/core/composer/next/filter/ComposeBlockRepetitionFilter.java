02587deb371387a3a56123a82bb7d7675a6e198c
pavel yurkin <pavel.yurkin@leroymerlin.ru>
2017-03-29 11:31:47 +0300

    fix getRepetitions

--- core/src/main/java/ru/pavelyurkin/musiccomposer/core/composer/next/filter/ComposeBlockRepetitionFilter.java
+++ core/src/main/java/ru/pavelyurkin/musiccomposer/core/composer/next/filter/ComposeBlockRepetitionFilter.java
@@ -25,7 +25,7 @@ public class ComposeBlockRepetitionFilter extends AbstractComposeBlockFilter {
 		return possibleNextComposeBlocks.stream().filter( composeBlock -> {
 			List<MusicBlock> musicBlocksToCheck = new ArrayList<>( previousBlocks );
 			musicBlocksToCheck.add( composeBlock.getMusicBlock() );
-			Map<Double, Integer> rhythmValueRepetitions = getRepetitions( musicBlocksToCheck );
+			Map<Double, Integer> rhythmValueRepetitions = getRepetitions( musicBlocksToCheck, 2 * WHOLE_NOTE );
 			for ( Map.Entry<Double, Integer> rhythmValueRepetition : rhythmValueRepetitions.entrySet() ) {
 				if ( rhythmValueRepetition.getValue() > getMaxNumberOfRepetitions( rhythmValueRepetition.getKey() ) ) {
 					return false;
@@ -35,8 +35,14 @@ public class ComposeBlockRepetitionFilter extends AbstractComposeBlockFilter {
 		} ).collect( Collectors.toList() );
 	}
 
-	Map<Double, Integer> getRepetitions( List<MusicBlock> musicBlocksToCheck ) {
-		// Rhtyhm values and number of actual repetitions ouput map
+	/**
+	 * Returns number of repetitions for different rhythm values, searching summary within range
+	 * @param musicBlocksToCheck
+	 * @param range
+	 * @return
+	 */
+	Map<Double, Integer> getRepetitions( List<MusicBlock> musicBlocksToCheck, double range ) {
+		// Rhythm values and number of actual repetitions ouput map
 		Map<Double, Integer> outputRepetitionMap = new HashMap<>(  );
 		// list represents repetition
 		List<MusicBlock> repetition = new ArrayList<>(  );
@@ -44,23 +50,28 @@ public class ComposeBlockRepetitionFilter extends AbstractComposeBlockFilter {
 		List<MusicBlock> musicBlocks = new ArrayList<>( musicBlocksToCheck );
 		// Repetition can consist of 1 to N music blocks. Iterating through them trying to count number of repetitions
 		for ( int blocksInRepetition = 1; blocksInRepetition < musicBlocksToCheck.size(); blocksInRepetition++ ) {
-			repetition.add( musicBlocksToCheck.get( musicBlocksToCheck.size() - blocksInRepetition ) );
+			// inserting at first place
+			repetition.add( 0, musicBlocksToCheck.get( musicBlocksToCheck.size() - blocksInRepetition ) );
 			musicBlocks.remove( musicBlocksToCheck.size() - blocksInRepetition );
-			if ( repetition.stream().mapToDouble( MusicBlock::getRhythmValue ).sum() < 2 * WHOLE_NOTE ) {
+			if ( repetition.stream().mapToDouble( MusicBlock::getRhythmValue ).sum() < range ) {
 				// We need preform several comparisons. For example here (1)(2)(3) (1)(2)(3) | (1)(2)(3) we need to compare last 3 notes with second and first
 				// Also number of comparison when cycle will break will show us number of repetitions
 				for ( int numberOfComparison = 0; numberOfComparison < musicBlocks.size() / repetition.size(); numberOfComparison++ ) {
 					// In each comparison we need to compare all blocks in the repetition with corresponding blocks in musicBlock list
 					// (1)(2)(3) (1)(2)(3) | (1)(2)(3)
-					//     |		 |			 |
-					//   				comparing
-					for ( int numberOfBlockInRepetition = 0; numberOfBlockInRepetition < repetition.size(); numberOfBlockInRepetition++ ) {
-						if ( !melodyExactEquality( repetition.get( numberOfBlockInRepetition ), musicBlocks.get( musicBlocks.size() - ( numberOfComparison + 1 ) * repetition.size() + numberOfBlockInRepetition ) ) ) {
-							if ( numberOfBlockInRepetition != 0 ) {
-								outputRepetitionMap.put( repetition.stream().mapToDouble( MusicBlock::getRhythmValue ).sum(), numberOfBlockInRepetition );
-							}
-							break;
-						}
+					//				|			|
+					//				------------
+					//				0 comparison
+					//     |		 			|
+					//	   ----------------------
+					//       1 comparison
+					List<MusicBlock> musicBlocksToCompare = musicBlocks.subList( musicBlocks.size() - ( numberOfComparison + 1 ) * repetition.size()
+							, musicBlocks.size() - ( numberOfComparison ) * repetition.size() );
+					if ( melodyExactEquality( repetition, musicBlocksToCompare ) ) {
+						outputRepetitionMap.put( repetition.stream().mapToDouble( MusicBlock::getRhythmValue ).sum(), numberOfComparison + 2 );
+					} else {
+						// next comparison makes sense only if previous is succeeded
+						break;
 					}
 				}
 			} else {
@@ -70,10 +81,11 @@ public class ComposeBlockRepetitionFilter extends AbstractComposeBlockFilter {
 		return outputRepetitionMap;
 	}
 
-	private boolean melodyExactEquality( MusicBlock firstMusicBlock, MusicBlock secondMusicBlock ) {
-		return isExactEquals( firstMusicBlock.getMelodyList(), secondMusicBlock.getMelodyList() );
+	private boolean melodyExactEquality( List<MusicBlock> firstMusicBlocks, List<MusicBlock> secondMusicBlocks ) {
+		return isExactEquals( new MusicBlock( firstMusicBlocks ).getMelodyList(), new MusicBlock( secondMusicBlocks ).getMelodyList() );
 	}
 
+	// TODO parametrise it
 	int getMaxNumberOfRepetitions( double rhythmValue ) {
 		if ( compare( rhythmValue, EIGHTH_NOTE ) <= 0 )
 			return 6;
--- core/src/test/java/ru/pavelyurkin/musiccomposer/core/composer/next/filter/ComposeBlockRepetitionFilterTest.java
+++ core/src/test/java/ru/pavelyurkin/musiccomposer/core/composer/next/filter/ComposeBlockRepetitionFilterTest.java
@@ -6,29 +6,119 @@ import ru.pavelyurkin.musiccomposer.core.model.MusicBlock;
 import ru.pavelyurkin.musiccomposer.core.model.melody.Melody;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import static jm.constants.Pitches.*;
 import static jm.constants.Durations.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class ComposeBlockRepetitionFilterTest {
 
 	private ComposeBlockRepetitionFilter composeBlockRepetitionFilter = new ComposeBlockRepetitionFilter();
 
+	// TODO finish after filter refactoring
+//	public void filterOutEighthNoteRepetition() {
+//		List<MusicBlock> blocks = Arrays.asList(
+//				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, EIGHTH_NOTE ) ) ), null ),
+//				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, EIGHTH_NOTE ) ) ), null ),
+//				new MusicBlock( Collections.singletonList( new Melody( new Note( C3, EIGHTH_NOTE ) ) ), null ),
+//				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, EIGHTH_NOTE ) ) ), null ),
+//				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, EIGHTH_NOTE ) ) ), null ),
+//				new MusicBlock( Collections.singletonList( new Melody( new Note( C3, EIGHTH_NOTE ) ) ), null )
+//		);
+//		composeBlockRepetitionFilter.filterIt(
+//				Collections.singletonList( new MusicBlock( Collections.singletonList( new Melody( new Note( C1, EIGHTH_NOTE ) ) ), null ) ),
+//				blocks
+//		)
+//	}
+
+	// getRepetitions tests
 	@Test
-	public void getRepetitions() throws Exception {
+	public void repetitionsInOnePitchBlocks() throws Exception {
 		List<MusicBlock> onePitchBlock = Arrays.asList(
-				new MusicBlock( Arrays.asList( new Melody( new Note( C1, EIGHTH_NOTE ), new Note( C1, EIGHTH_NOTE ) ) ), null ),
-				new MusicBlock( Arrays.asList( new Melody( new Note( C1, EIGHTH_NOTE ), new Note( C1, EIGHTH_NOTE ) ) ), null ),
-				new MusicBlock( Arrays.asList( new Melody( new Note( C1, EIGHTH_NOTE ), new Note( C1, EIGHTH_NOTE ) ) ), null ),
-				new MusicBlock( Arrays.asList( new Melody( new Note( C1, EIGHTH_NOTE ), new Note( C1, EIGHTH_NOTE ) ) ), null )
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, EIGHTH_NOTE ), new Note( C1, EIGHTH_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, EIGHTH_NOTE ), new Note( C1, EIGHTH_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, EIGHTH_NOTE ), new Note( C1, EIGHTH_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, EIGHTH_NOTE ), new Note( C1, EIGHTH_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, EIGHTH_NOTE ), new Note( C1, EIGHTH_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, EIGHTH_NOTE ), new Note( C1, EIGHTH_NOTE ) ) ), null )
 		);
-		Map<Double, Integer> repetitions0 = composeBlockRepetitionFilter.getRepetitions( onePitchBlock );
-		assertEquals( 2, repetitions0.size() );
-		assertTrue( repetitions0.get( HALF_NOTE ) == 4 );
-		assertTrue( repetitions0.get( WHOLE_NOTE ) == 2 );
+		Map<Double, Integer> repetitions0 = composeBlockRepetitionFilter.getRepetitions( onePitchBlock, 2 * WHOLE_NOTE );
+		assertThat( 3, is( equalTo( repetitions0.size() ) ) );
+		assertThat( 6, is( equalTo( repetitions0.get( QUARTER_NOTE ) ) ) );
+		assertThat( 3, is( equalTo( repetitions0.get( HALF_NOTE ) ) ) );
+		assertThat( 2, is( equalTo( repetitions0.get( DOTTED_HALF_NOTE ) ) ) );
+	}
+
+	@Test
+	public void repetitionsOfThreeNotes() throws Exception {
+		List<MusicBlock> blocks = Arrays.asList(
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C3, QUARTER_NOTE ) ) ), null ),
+
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C3, QUARTER_NOTE ) ) ), null ),
+
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C3, QUARTER_NOTE ) ) ), null )
+		);
+		Map<Double, Integer> repetitions0 = composeBlockRepetitionFilter.getRepetitions( blocks, 2 * WHOLE_NOTE );
+		assertThat( 1, is( equalTo( repetitions0.size() ) ) );
+		assertThat( 3, is( equalTo( repetitions0.get( DOTTED_HALF_NOTE ) ) ) );
+	}
+
+	@Test
+	public void zeroRepetitionsOfTwoNotesIfWrongRhythmValue() throws Exception {
+		List<MusicBlock> blocks = Arrays.asList(
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, QUARTER_NOTE ) ) ), null ),
+
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, DOTTED_QUARTER_NOTE ) ) ), null ),
+
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, QUARTER_NOTE ) ) ), null )
+		);
+		Map<Double, Integer> repetitions0 = composeBlockRepetitionFilter.getRepetitions( blocks, 2 * WHOLE_NOTE );
+		assertThat( 0, is( equalTo( repetitions0.size() ) ) );
+	}
+
+	@Test
+	public void repetitionsOfTwoNotesOutOfBounds() throws Exception {
+		List<MusicBlock> blocks = Arrays.asList(
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, QUARTER_NOTE ) ) ), null ),
+
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, QUARTER_NOTE ) ) ), null ),
+
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, QUARTER_NOTE ) ) ), null ),
+
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, QUARTER_NOTE ) ) ), null ),
+
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, QUARTER_NOTE ) ) ), null ),
+
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, QUARTER_NOTE ) ) ), null ),
+
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C1, QUARTER_NOTE ) ) ), null ),
+				new MusicBlock( Collections.singletonList( new Melody( new Note( C2, QUARTER_NOTE ) ) ), null )
+		);
+		Map<Double, Integer> repetitions0 = composeBlockRepetitionFilter.getRepetitions( blocks, 2 * WHOLE_NOTE );
+		assertThat( 3, is( equalTo( repetitions0.size() ) ) );
+		assertThat( 7, is( equalTo( repetitions0.get( HALF_NOTE ) ) ) );
+		assertThat( 3, is( equalTo( repetitions0.get( WHOLE_NOTE ) ) ) );
+		assertThat( 2, is( equalTo( repetitions0.get( WHOLE_NOTE + HALF_NOTE ) ) ) );
 	}
 
 }
