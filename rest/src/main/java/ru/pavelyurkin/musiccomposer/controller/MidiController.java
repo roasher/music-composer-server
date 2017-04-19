7d184e30e5bb2fde3248306c3ee5ed2a58c03352
pavel yurkin <pavel.yurkin@leroymerlin.ru>
2017-04-19 18:16:32 +0300

    add swagger for api

--- rest/src/main/java/ru/pavelyurkin/musiccomposer/controller/MidiController.java
+++ rest/src/main/java/ru/pavelyurkin/musiccomposer/controller/MidiController.java
@@ -1,5 +1,7 @@
-package ru.pavelyurkin.musiccomposer.rest;
+package ru.pavelyurkin.musiccomposer.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,16 +13,25 @@ import ru.pavelyurkin.musiccomposer.core.service.ComposeService;
 import ru.pavelyurkin.musiccomposer.dto.CompositionDTO;
 
 @RestController
-public class MidiProvider {
+public class MidiController {
+
+	private final ComposeService composeService;
+
+	private final CompositionConverter compositionConverter;
 
 	@Autowired
-	private ComposeService composeService;
+	public MidiController( ComposeService composeService, CompositionConverter compositionConverter ) {
+		this.composeService = composeService;
+		this.compositionConverter = compositionConverter;
+	}
 
-	@Autowired
-	private CompositionConverter compositionConverter;
-
+	@ApiOperation( "Note generation" )
 	@RequestMapping( path = "/getBars", method = RequestMethod.GET )
-	public CompositionDTO getNotes( @RequestParam String compositionId, @RequestParam int numberOfBars ) {
+	public CompositionDTO getNotes(
+			@ApiParam(name = "compositionId", required = true, value = "Unique Id of generation session")
+			@RequestParam String compositionId,
+			@ApiParam(name = "numberOfBars", required = true, value = "Number of bars that you wan't to be generated")
+			@RequestParam int numberOfBars ) {
 		Composition nextBarsFromComposition = composeService.getNextBarsFromComposition( compositionId, numberOfBars );
 		return compositionConverter.convert( nextBarsFromComposition );
 	}
