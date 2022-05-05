3d59e0a4296784892e523efebc7a455bbd6193a6
pavel yurkin <woodythegreat@yandex.ru>
2022-05-05 15:48:53 +0300

    change bach filter

--- core/src/main/java/ru/pavelyurkin/musiccomposer/core/composer/next/filter/custom/BachChoralFilter.java
+++ core/src/main/java/ru/pavelyurkin/musiccomposer/core/composer/next/filter/custom/BachChoralFilter.java
@@ -5,8 +5,7 @@ import ru.pavelyurkin.musiccomposer.core.composer.next.filter.*;
 
 import java.util.Arrays;
 
-import static jm.constants.Durations.QUARTER_NOTE;
-import static jm.constants.Durations.WHOLE_NOTE;
+import static jm.constants.Durations.*;
 import static jm.constants.Pitches.*;
 
 /**
@@ -17,8 +16,8 @@ public class BachChoralFilter implements ComposeStepFilter {
     @Delegate
 	private ComposeStepFilter composeStepFilter = new ComposeStepRepetitionFilter(
 			new ComposeStepVarietyFilter( -1, 6,
-					new KeyVarietyFilter( 1, 2 * WHOLE_NOTE,
-							new ComposeStepRestFilter( QUARTER_NOTE,
+					new KeyVarietyFilter( 1, 4 * WHOLE_NOTE,
+							new ComposeStepRestFilter( EIGHTH_NOTE,
 									new ComposeStepVoiceRangeFilter( Arrays.asList(
 										new ComposeStepVoiceRangeFilter.Range( C4, C6 ),
 										new ComposeStepVoiceRangeFilter.Range( F3, F5 ),
