bf14bb33b47ee22adfb6619d2a8e7788cdead948
Pavel Yurkin <pavelyurkin@yandex-team.ru>
2024-01-02 19:54:08 +0300

    iter

--- load-test/src/main/java/MusicComposer/Main.java
+++ load-test/src/main/java/MusicComposer/Main.java
@@ -4,7 +4,7 @@ import java.util.stream.IntStream;
 
 public class Main {
   public static void main(String[] args) {
-    IntStream.range(0, 30).mapToObj(i -> new Thread(new CompositionProcess(1)))
+    IntStream.range(0, 10).mapToObj(i -> new Thread(new CompositionProcess(1)))
         .forEach(Thread::start);
   }
 }
